2af34b812c0bcfe2495c26009a100bba45c78cb4
“ken” <112563332@qq.com>
2021-12-22 20:13:29 +0800

    1.测试jenkins

---
+++ src/main/java/com/lenovo/mycode/controller/JenkinsController.java
@@ -0,0 +1,17 @@
+package com.lenovo.mycode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/jenkins")
+@RestController
+public class JenkinsController {
+
+    @GetMapping("/jenkins")
+    public String myJenkins(@RequestParam("msg") String msg) {
+        return msg;
+    }
+
+}
